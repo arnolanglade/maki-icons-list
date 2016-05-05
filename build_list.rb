715cbbd1329390c360dc3f023c0f824e3b9b3e1e
Maurizio De Magnis <root@olisti.co>
2016-05-05 10:32:02 +0200

    Create build_list.rb

---
+++ build_list.rb
@@ -0,0 +1,32 @@
+require "open-uri"
+require "json"
+
+def raw_icon_url(path:)
+  "https://cdn.rawgit.com/mapbox/maki/master/#{path}"
+end
+
+url = "https://api.github.com/repos/mapbox/maki/contents/icons"
+response = JSON.parse(open(url).read)
+
+icons = response.
+  map do |item|
+    {
+      name: File.basename(item["name"], File.extname(item["name"])),
+      path: item["path"],
+    }
+  end.select do |icon|
+    icon[:path].match(/.*15\.svg$/)
+  end.sort_by do |icon|
+    icon[:name]
+  end
+
+list = icons.map do |icon|
+  {
+    name: icon[:name].match(/([a-z\-]+)+\-[0-9]+/)[1],
+    url: raw_icon_url(path: icon[:path]),
+  }
+end
+
+list.each do |icon|
+  puts "- #{icon[:name]} ![#{icon[:name]}](#{icon[:url]})"
+end
